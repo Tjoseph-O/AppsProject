ddb574b8117aae2d8e221d3b0da26e2063e99b2a
Temitope Joseph <126495757+Tjoseph-O@users.noreply.github.com>
2023-06-17 23:05:39 +0100

    first commit

---
+++ test/bankApp/AccountTest.java
@@ -0,0 +1,11 @@
+package bankApp;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AccountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
